741c17d0ff819bd08b34d7a1e9f5c60a28ad528c
nicolashornuel <75623321+nicolashornuel@users.noreply.github.com>
2023-04-26 15:35:45 +0000

    firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,11 @@
+import * as functions from "firebase-functions";
+
+// import { defineBool } from "firebase-functions/params";
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
