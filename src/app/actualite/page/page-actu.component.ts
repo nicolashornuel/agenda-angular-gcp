4e0a782fc8702f32ec81b17457c79e4d41b63c09
nicolashornuel <75623321+nicolashornuel@users.noreply.github.com>
2024-11-27 16:56:04 +0000

    card actu

--- src/app/actualite/page/page-actu.component.ts
+++ src/app/actualite/page/page-actu.component.ts
@@ -35,32 +35,12 @@ export class PageActuComponent implements OnInit {
       name: 'usinenouvelle'
     },
     {
-      url: 'https://www.courrierinternational.com/feed/all/rss.xml',
-      name: 'courrierinternational'
-    },
-    {
-      url: 'https://www.tomsguide.fr/feed/',
-      name: 'tomsguide'
-    },
-    {
-      url: 'https://www.generation-nt.com/export/rss.xml',
-      name: 'generation-nt'
-    },
-    {
       url: 'https://www.sciencesetavenir.fr/rss.xml',
       name: 'sciencesetavenir'
     },
     {
       url: 'https://www.journaldunet.com/rss/',
       name: 'journaldunet'
-    },
-    {
-      url: 'https://korben.info/feed',
-      name: 'korben'
-    },
-    {
-      url: 'https://www.tomshardware.fr/feed/',
-      name: 'tomshardware'
     }
   ];
 
