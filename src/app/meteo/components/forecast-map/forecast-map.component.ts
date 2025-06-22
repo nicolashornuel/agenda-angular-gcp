7911c8747703acc66cea0b528dc88676348d17a7
Nicolas HORNUEL <nicolas.hornuel@gmail.com>
2025-06-22 20:31:08 +0200

    textarea memo - iframe meteo

--- src/app/meteo/components/forecast-map/forecast-map.component.ts
+++ src/app/meteo/components/forecast-map/forecast-map.component.ts
@@ -9,10 +9,32 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class ForecastMapComponent implements OnInit{
 public src?: SafeResourceUrl;
 private _sanitizer = inject(DomSanitizer);
+private params = {
+  zoom: 5,
+  detail: true,
+  message: true,
+  type: 'map', //  map | forecast
+  location: 'coordinates',
+  metricRain: 'mm',
+  metricTemp: '°C',
+  metricWind: 'km/h',
+  overlay: 'wind',
+  product: 'ecmwf',
+  level: 'surface',
+};
 
   async ngOnInit(): Promise<void> {
     const { coords }: GeolocationPosition = await this.getCurrentPositionAsync();
-    const src = `https://embed.windy.com/embed2.html?lat=${coords.latitude}&lon=${coords.longitude}&zoom=5`;
+    let src = `https://embed.windy.com/embed2.html?`;
+    const params = new URLSearchParams(
+      Object.entries(this.params).reduce((acc, [key, value]) => {
+        acc[key] = value.toString();
+        return acc;
+      }, {} as Record<string, string>)
+    );
+    params.set('detailLat', coords.latitude.toFixed(3));
+    params.set('detailLon', coords.longitude.toFixed(3));
+    src += `&${params.toString()}`;
     this.src = this._sanitizer.bypassSecurityTrustResourceUrl(src);
   }
 
@@ -20,3 +42,5 @@ private _sanitizer = inject(DomSanitizer);
     return new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
   }
 }
+
+
